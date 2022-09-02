6d414d11acb52f589fffca8e134ccf6674cebbd3
DougS Silva <det060903@gmail.com>
2022-09-02 10:49:02 -0300

    Configuração do EntityFramework

---
+++ aspnetcore.api-pucminas/Models/ApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace aspnetcore.api_pucminas.Models
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions options) : base(options)   
+        {
+        }
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Consumo> Consumos{ get; set; }
+    }
+}
--- aspnetcore.api-pucminas/Models/Consumo.cs
+++ aspnetcore.api-pucminas/Models/Consumo.cs
@@ -13,6 +13,7 @@ namespace aspnetcore.api_pucminas.Models
         [Required]
         public DateTime Data { get; set; }
         [Required]
+        [Column(TypeName ="decimal(18,2)")]
         public decimal Valor { get; set; }
         [Required]
         public TipoCombustivel Tipo { get; set; }
