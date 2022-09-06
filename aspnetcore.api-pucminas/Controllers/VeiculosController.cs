21c81cfa0b49a8cead7e1490799da417b6bda411
DougS Silva <det060903@gmail.com>
2022-09-06 09:43:50 -0300

    Inserido Put e Delete

--- aspnetcore.api-pucminas/Controllers/VeiculosController.cs
+++ aspnetcore.api-pucminas/Controllers/VeiculosController.cs
@@ -24,18 +24,52 @@ namespace aspnetcore.api_pucminas.Controllers
         [HttpPost]
         public async Task<ActionResult> Create(Veiculo model)
         {
-            if(model.AnoFabricacao <=0 || model.AnoModelo <= 0)
+            if (model.AnoFabricacao <= 0 || model.AnoModelo <= 0)
             {
                 return BadRequest(new { message = "Obrigatorio Ano do modelo e Ano fabricação" });
             }
             _context.Veiculos.Add(model);
             await _context.SaveChangesAsync();
 
-            return Ok(model);
+            return CreatedAtAction("GetById", new { id = model.Id }, model);
         }
 
         [HttpGet("id")]
-        public asy MyProperty { get; set; }
+        public async Task<ActionResult> GetById(int id)
+        {
+            var model = await _context.Veiculos
+                .FirstOrDefaultAsync(c => c.Id == id);
 
+            if (model == null) return NotFound();
+
+            return Ok(model);
+        }
+
+        [HttpPut("{id")]
+        public async Task<ActionResult> Update(int id, Veiculo model)
+        {
+            if (id != model.Id) return BadRequest();
+
+            var modelDb = await _context.Veiculos.AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == id);
+            if (modelDb == null) return NotFound();
+
+            _context.Veiculos.Update(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var model = await _context.Veiculos.FindAsync(id);
+
+            if (model == null) return NotFound();
+
+            _context.Veiculos.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+         }
     }
 }
